198e2bfc6f4cc4d91c69c83bd98b89072e23c545
Rustambek Turganov <aaa_ppp_1989@mail.ru>
2023-01-19 13:40:06 +0600

    add function generateRandomColor()

--- Colors/app.js
+++ Colors/app.js
@@ -1,7 +1,14 @@
 const cols = document.querySelectorAll(".col");
 
 function generateRandomColor() {
+    const hexCodex = '0123456789ABCDEF';
+    let color = '';
     
+    for (let i = 0; i < 6; i++) {
+        color += hexCodex[Math.floor(Math.random() * hexCodex.length)]; 
+    }
+
+    return '#' + color;
 }
 
 function setRandomColors() {
